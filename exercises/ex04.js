56b11cd4959c142db3a2387685abde2fdba40939
Makani <tashamglass@gmail.com>
2025-10-16 18:31:11 -0700

    10/16 exercise.

--- exercises/ex04.js
+++ exercises/ex04.js
@@ -1,6 +1,11 @@
 let count = 0;
+let colours = ["Orchid", "Coral", "HotPink", "Plum"];
+colours.push("SeaGreen")
+colours.unshift("RosyBrown");
+let coloursSize = colours.length;
 $("#needy-button").click( function() {
+    $("#needy-button").html("Clicks: " + count + " Colour: " + colours[count % (coloursSize)]);
+    $("body").css("background-color", (colours[count % (coloursSize)]));
     count += 1;
-    $("#needy-button").html("You clicked me " + count + " times.");
-
 });
+
